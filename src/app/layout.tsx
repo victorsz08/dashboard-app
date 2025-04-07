eb02b60b538ea1f007e49a777d2fd183901501ac
victorsz08 <joaovictorsal777@gmail.com>
2025-04-07 11:05:08 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={` antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
